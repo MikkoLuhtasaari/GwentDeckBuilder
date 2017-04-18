e2a988b93db05ccf3c2e42dc8925f45bf182d857
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-04-18 16:40:06 +0300

    Refactor code

--- app/src/main/java/mikkoluhtasaari/gwentdeckbuilder/MainActivity.java
+++ app/src/main/java/mikkoluhtasaari/gwentdeckbuilder/MainActivity.java
@@ -6,10 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 import java.io.IOException;
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 import okhttp3.MediaType;
@@ -22,24 +20,27 @@ public class MainActivity extends AppCompatActivity {
     MediaType JSON;
 
     public static final String baseUrl = "https://api.gwentapi.com/v0";
-    ArrayList<Card> cards;
-    ArrayList<TestCard> testCards;
+    public static final String[] factionUrls = new String[6];
+    public static final String offset = "?limit=200&offset=0";
+    ArrayList<TestCard> neutralCards;
+    ResultSet cardUrls;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        cards = new ArrayList<>();
-        testCards = new ArrayList<>();
+        neutralCards = new ArrayList<>();
+        factionUrls[0] = "9aD4AoKlRhqKxQ2b7F0JOA";
+        factionUrls[1] = "C21SnrUdSSW7ttfGNkOzeA";
+        factionUrls[2] = "DuOHJOMpTWSevegxs7xOKQ";
+        factionUrls[3] = "sa1zdVBdST6FqFJUY1vIcQ";
+        factionUrls[4] = "vVL5p_u6SRmotqThkahITA";
+        factionUrls[5] = "wkY8HZJATUKd_EtraBoC3A";
         setContentView(R.layout.activity_main);
 
         client = new OkHttpClient();
         JSON = MediaType.parse("application/json; charset=utf-8");
 
     }
-    public void makeGetRequest(View v) throws IOException {
-
-    }
-
 
     public void testHttp(View view) throws IOException {
         GetTask task = new GetTask();
@@ -53,12 +54,18 @@ public class MainActivity extends AppCompatActivity {
 
             try {
                 System.out.println("doInBackground");
-                //String getResponse = get("https://api.gwentapi.com/v0/cards/QdjB4mwISJaAT7j5rWhZdA");
-                String getResponse = get("https://api.gwentapi.com/v0/cards?limit=500&offset=0");
+                String getResponse = get("https://api.gwentapi.com/v0/cards/factions/"+factionUrls[1]+offset);
                 System.out.println(getResponse);
                 Gson gson = new Gson();
-                //TestCard card = gson.fromJson(getResponse, TestCard.class);
-                //System.out.println(card);
+                cardUrls = gson.fromJson(getResponse, ResultSet.class);
+
+                for (int i = 0; i < cardUrls.getResults().size(); i++) {
+                    String temp = get(cardUrls.getResults().get(i).getHref());
+                    neutralCards.add(gson.fromJson(temp, TestCard.class));
+                    System.out.println("haetaan");
+                }
+                System.out.println("ready");
+                System.out.println(neutralCards.size());
                 return getResponse;
             } catch (Exception e) {
                 System.out.println("Exception");
@@ -67,9 +74,9 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        protected void onPostExecute(String getResponse) {
+        /*protected void onPostExecute(String getResponse) {
             System.out.println(getResponse);
-        }
+        }*/
 
         public String get(String url) throws IOException {
             Request request = new Request.Builder()
