3b2f9307fa113dc10d876fb0c2d9c304981761d4
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-04-18 19:31:23 +0300

    Refactor code

--- app/src/main/java/mikkoluhtasaari/gwentdeckbuilder/SelectFactionActivity.java
+++ app/src/main/java/mikkoluhtasaari/gwentdeckbuilder/SelectFactionActivity.java
@@ -1,107 +1,51 @@
 package mikkoluhtasaari.gwentdeckbuilder;
 
-import android.content.pm.ActivityInfo;
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.WindowManager;
 
-import com.google.gson.Gson;
-
-import java.io.IOException;
 import java.util.ArrayList;
 
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-
 public class SelectFactionActivity extends AppCompatActivity {
-    OkHttpClient client;
-    MediaType JSON;
 
     public static final String baseUrl = "https://api.gwentapi.com/v0";
     public static final String[] factionUrls = new String[6];
     public static final String offset = "?limit=200&offset=0";
     ArrayList<Card> neutralCards;
-    ResultSet cardUrls;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        System.out.println("onCreate");
 
         neutralCards = new ArrayList<>();
+        //Monsters
         factionUrls[0] = "9aD4AoKlRhqKxQ2b7F0JOA";
+        //Neutrals
         factionUrls[1] = "C21SnrUdSSW7ttfGNkOzeA";
+        //Nilfgaard
         factionUrls[2] = "DuOHJOMpTWSevegxs7xOKQ";
+        //NR
         factionUrls[3] = "sa1zdVBdST6FqFJUY1vIcQ";
+        //Scoiatel
         factionUrls[4] = "vVL5p_u6SRmotqThkahITA";
+        //Skellige
         factionUrls[5] = "wkY8HZJATUKd_EtraBoC3A";
 
         setContentView(R.layout.activity_select_faction);
         //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
-        client = new OkHttpClient();
-        JSON = MediaType.parse("application/json; charset=utf-8");
 
-        /*if(getIntent() != null) {
-            neutralCards = (ArrayList<Card>) getIntent().getExtras().get("neutralCards");
-        }*/
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
-            System.out.println("Extras");
-            neutralCards = (ArrayList<Card>)extras.getSerializable("neutralCards");
+            neutralCards = (ArrayList<Card>) extras.getSerializable("neutralCards");
         }
-        System.out.println(neutralCards.size());
-
-        //GetTask task = new GetTask();
-        //task.execute();
+        System.out.println("Neutrals size "+neutralCards.size());
 
     }
 
-    public void openMonsters(View view) throws IOException {
-        GetTask task = new GetTask();
-        task.execute();
-    }
-
-    public class GetTask extends AsyncTask {
-        private Exception exception;
-        @Override
-        protected Object doInBackground(Object[] params) {
-
-            try {
-                System.out.println("doInBackground");
-                String getResponse = get("https://api.gwentapi.com/v0/cards/factions/"+factionUrls[1]+offset);
-                System.out.println(getResponse);
-                Gson gson = new Gson();
-                cardUrls = gson.fromJson(getResponse, ResultSet.class);
-
-                for (int i = 0; i < cardUrls.getResults().size(); i++) {
-                    String temp = get(cardUrls.getResults().get(i).getHref());
-                    neutralCards.add(gson.fromJson(temp, Card.class));
-                    System.out.println("haetaan");
-                }
-                System.out.println("ready");
-                System.out.println(neutralCards.size());
-                return getResponse;
-            } catch (Exception e) {
-                System.out.println("Exception");
-                this.exception = e;
-                return null;
-            }
-        }
-
-        public String get(String url) throws IOException {
-            Request request = new Request.Builder()
-                    .url(url)
-                    .build();
-
-            Response response = client.newCall(request).execute();
-            return response.body().string();
-        }
+    public void openMonsters(View view) {
+        //TODO new loading activity
     }
 
 }
