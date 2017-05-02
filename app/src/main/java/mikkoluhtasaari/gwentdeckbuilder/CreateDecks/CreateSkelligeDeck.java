4d55c94fd43a04e4fc6415dba72252b358204a1b
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-05-02 16:55:11 +0300

    Fix back button

--- app/src/main/java/mikkoluhtasaari/gwentdeckbuilder/CreateDecks/CreateSkelligeDeck.java
+++ app/src/main/java/mikkoluhtasaari/gwentdeckbuilder/CreateDecks/CreateSkelligeDeck.java
@@ -1,5 +1,6 @@
 package mikkoluhtasaari.gwentdeckbuilder.CreateDecks;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,6 +8,7 @@ import java.util.ArrayList;
 
 import mikkoluhtasaari.gwentdeckbuilder.Card;
 import mikkoluhtasaari.gwentdeckbuilder.R;
+import mikkoluhtasaari.gwentdeckbuilder.SelectFactionActivity;
 
 public class CreateSkelligeDeck extends AppCompatActivity {
 
@@ -31,5 +33,12 @@ public class CreateSkelligeDeck extends AppCompatActivity {
         }
         System.out.println("neutral size " + neutralCards.size());
         System.out.println("skellige size " +skelligeCards.size());
+    }
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(this.getApplicationContext(), SelectFactionActivity.class);
+        intent.putExtra("neutralCards",neutralCards);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        this.getApplicationContext().startActivity(intent);
     }
 }
