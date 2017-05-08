264a7b1c215f766c14d87e30ed6f3932480b4ae8
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-05-08 11:48:19 +0300

    Add javadoc

--- app/src/main/java/mikkoluhtasaari/gwentdeckbuilder/LoadClasses/LoadOther.java
+++ app/src/main/java/mikkoluhtasaari/gwentdeckbuilder/LoadClasses/LoadOther.java
@@ -22,19 +22,60 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
+/**
+ * Loads class spesific cards from server.
+ *
+ * @author Mikko Luhtasaari
+ * @version 1.0, 25 Apr 2017
+ * @since 1.0
+ */
 public class LoadOther extends AppCompatActivity {
 
+    /**
+     * Contains cards from previous loaders.
+     */
     ArrayList<Card> neutralCards;
+
+    /**
+     * Contains the retrieved cards.
+     */
     ArrayList<Card> otherCards;
+
+    /**
+     * Contains the urls from which cards are retrieved.
+     */
     ResultSet cardUrls;
 
+    /**
+     * Contains part of the url.
+     */
     String factionUrl;
+
+    /**
+     * Contains part of the url.
+     */
     private final String baseUrl = "https://api.gwentapi.com/v0/cards/factions/";
+
+    /**
+     * Contains part of the url.
+     */
     private final String offset = "?limit=200&offset=0";
 
+    /**
+     * Makes internet connection easier.
+     */
     OkHttpClient client;
+
+    /**
+     * Makes parsing data easier.
+     */
     MediaType JSON;
 
+    /**
+     * Creates the view.
+     *
+     * @param savedInstanceState bundle savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,7 +101,9 @@ public class LoadOther extends AppCompatActivity {
         new GetTask(this).execute();
     }
 
-
+    /**
+     * Handles backend connection.
+     */
     public class GetTask extends AsyncTask<URL, Integer, Long> {
         private Exception exception;
         private Context context;
@@ -69,6 +112,11 @@ public class LoadOther extends AppCompatActivity {
             this.context = context.getApplicationContext();
         }
 
+        /**
+         * Retrieves data from backend.
+         * @param params Url
+         * @return returns length of the retrieved and parsed data.
+         */
         @Override
         protected Long doInBackground(URL... params) {
             try {
@@ -76,8 +124,15 @@ public class LoadOther extends AppCompatActivity {
                 String getResponse = get(baseUrl+factionUrl+offset);
                 System.out.println(getResponse);
                 Gson gson = new Gson();
+
+                /**
+                 * Parses resultset from initial data. The server first returns a set of card urls.
+                 */
                 cardUrls = gson.fromJson(getResponse, ResultSet.class);
 
+                /**
+                 * Gets cards from server with urls from resultset.
+                 */
                 for (int i = 0; i < cardUrls.getResults().size(); i++) {
                     String temp = get(cardUrls.getResults().get(i).getHref());
                     otherCards.add(gson.fromJson(temp, Card.class));
@@ -91,15 +146,31 @@ public class LoadOther extends AppCompatActivity {
             }
         }
 
+        /**
+         * Starts new activity when all cards are retrieved.
+         *
+         * @param result Long result
+         */
         protected void onPostExecute(Long result) {
             Intent intent = new Intent(context, CreateDeck.class);
             intent.putExtra("neutralCards",neutralCards);
             intent.putExtra("otherCards", otherCards);
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(intent);
+
+            /**
+             * Returning to loader activity is not allowed.
+             */
             finish();
         }
 
+        /**
+         * Creates requests.
+         *
+         * @param url url from which results are to be retrieved.
+         * @return Returns results as String
+         * @throws IOException Retrieving data might fail for various reasons.
+         */
         public String get(String url) throws IOException {
             Request request = new Request.Builder()
                     .url(url)
