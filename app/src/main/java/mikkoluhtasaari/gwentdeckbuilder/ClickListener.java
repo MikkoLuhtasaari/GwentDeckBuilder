2fdc999cdf04073a85fcfb684e9acfd50e87fbb8
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-05-08 12:25:58 +0300

    Add javadoc

--- app/src/main/java/mikkoluhtasaari/gwentdeckbuilder/ClickListener.java
+++ app/src/main/java/mikkoluhtasaari/gwentdeckbuilder/ClickListener.java
@@ -3,9 +3,12 @@ package mikkoluhtasaari.gwentdeckbuilder;
 import android.view.View;
 
 /**
- * Created by M1k1tus on 07-May-17.
+ * Interface for clickListener
+ *
+ * @author Mikko Luhtasaari
+ * @version 1.0, 07 May 2017
+ * @since 1.0
  */
-
 public interface ClickListener {
     void onClick(View view, int position);
 
