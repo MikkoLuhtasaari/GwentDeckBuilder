bdf3e4ee5da810c1ae17b4642c2c1e1bc75aa692
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-05-08 11:45:43 +0300

    Add javadoc

--- app/src/main/java/mikkoluhtasaari/gwentdeckbuilder/LoadClasses/LoadNeutrals.java
+++ app/src/main/java/mikkoluhtasaari/gwentdeckbuilder/LoadClasses/LoadNeutrals.java
@@ -22,14 +22,45 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
+/**
+ * Loads neutral cards from server on startup.
+ *
+ * @author Mikko Luhtasaari
+ * @version 1.0, 25 Apr 2017
+ * @since 1.0
+ */
 public class LoadNeutrals extends AppCompatActivity {
 
+    /**
+     * Contains the url from which the card urls are retrieved.
+     */
     public static final String neutralUrl = "https://api.gwentapi.com/v0/cards/factions/C21SnrUdSSW7ttfGNkOzeA?limit=200&offset=0";
+
+    /**
+     * Contains the retrieved cards.
+     */
     ArrayList<Card> neutralCards;
+
+    /**
+     * Contains the urls from which cards are retrieved.
+     */
     ResultSet cardUrls;
 
+    /**
+     * Makes internet connection easier.
+     */
     OkHttpClient client;
+
+    /**
+     * Makes parsing data easier.
+     */
     MediaType JSON;
+
+    /**
+     * Creates the view.
+     *
+     * @param savedInstanceState bundle savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +73,10 @@ public class LoadNeutrals extends AppCompatActivity {
             new LoadNeutrals.GetTask(this).execute();
     }
 
+
+    /**
+     * Handles backend connection.
+     */
     public class GetTask extends AsyncTask<URL, Integer, Long> {
         private Exception exception;
         private Context context;
@@ -50,20 +85,29 @@ public class LoadNeutrals extends AppCompatActivity {
             this.context = context.getApplicationContext();
         }
 
+        /**
+         * Retrieves data from backend.
+         * @param params Url
+         * @return returns length of the retrieved and parsed data.
+         */
         @Override
         protected Long doInBackground(URL... params) {
             try {
-                System.out.println("Fetching neutral cards");
                 String getResponse = get(neutralUrl);
-                System.out.println(getResponse);
                 Gson gson = new Gson();
+
+                /**
+                 * Parses resultset from initial data. The server first returns a set of card urls.
+                 */
                 cardUrls = gson.fromJson(getResponse, ResultSet.class);
 
+                /**
+                 * Gets cards from server with urls from resultset.
+                 */
                 for (int i = 0; i < cardUrls.getResults().size(); i++) {
                     String temp = get(cardUrls.getResults().get(i).getHref());
                     neutralCards.add(gson.fromJson(temp, Card.class));
                 }
-                System.out.println(neutralCards.size());
                 return (long)neutralCards.size();
             } catch (Exception e) {
                 System.out.println(e);
@@ -72,14 +116,31 @@ public class LoadNeutrals extends AppCompatActivity {
             }
         }
 
+        /**
+         * Starts new activity when all cards are retrieved.
+         *
+         * @param result Long result
+         */
+        @Override
         protected void onPostExecute(Long result) {
             Intent intent = new Intent(context, SelectFactionActivity.class);
             intent.putExtra("neutralCards",neutralCards);
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(intent);
+
+            /**
+             * Returning to loader activity is not allowed.
+             */
             finish();
         }
 
+        /**
+         * Creates requests.
+         *
+         * @param url url from which results are to be retrieved.
+         * @return Returns results as String
+         * @throws IOException Retrieving data might fail for various reasons.
+         */
         public String get(String url) throws IOException {
             Request request = new Request.Builder()
                     .url(url)
